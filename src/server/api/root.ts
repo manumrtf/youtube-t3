bcf5395711b662506a15095859cc9f3ce7f12120
manumrtf <manuelmartindev@outlook.com>
2023-03-08 10:45:32 +0100

    part 2 starter point

---
+++ src/server/api/root.ts
@@ -0,0 +1,16 @@
+import { createTRPCRouter } from "./trpc";
+import { exampleRouter } from "./routers/example";
+import { todosRouter } from "./routers/todos";
+
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here
+ */
+export const appRouter = createTRPCRouter({
+  example: exampleRouter,
+  todosRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
